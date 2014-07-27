5ad2d767f725b082c4f27bf9079f63980887a5d3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-07-27 17:30:22 +0200

    add event_calendar specs.

--- spec/kalindar_spec.rb
+++ spec/kalindar_spec.rb
@@ -9,3 +9,17 @@ describe Kalindar do
     expect(false).to eq(true)
   end
 end
+
+describe EventCalendar do
+  it 'parses an ics file' do
+    cal = EventCalendar.new 'spec/testcal.ics'
+  end
+
+  it 'finds events given date' do
+    cal = EventCalendar.new 'spec/testcal.ics'
+    events = cal.find_events (Date.new(2014, 07, 27))
+    event_names = events.map(&:summary)
+    expect(event_names.include? "onehour").to eq true
+    expect(event_names.include? "allday").to eq true
+  end
+end

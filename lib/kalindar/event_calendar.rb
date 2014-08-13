d37adc3b4dbc79d31ff0f41f3aca27e1240a23e3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-08-13 12:20:12 +0200

    add uuid-picking, comments to event_calendar.

--- lib/kalindar/event_calendar.rb
+++ lib/kalindar/event_calendar.rb
@@ -1,19 +1,29 @@
 require 'ri_cal'
 require 'kalindar/calendar'
 
+# Public facing methods should return Event Decorators,
+# private methods can return "raw" RiCal::Component::Event s.
 class EventCalendar
   attr_accessor :calendars
 
+  # Given filename or array of filenames, initialize the calendar.
   def initialize filename
     @calendars = []
     if filename.class == Array
-      filename.each {|file| read_file file}
+      filename.each {|file| read_file file, true}
     else
-      read_file filename
+      read_file filename, true
     end
   end
 
-  def read_file filename
+  # Opens calendar
+  # param: create if true, create empty calendar file if not existant.
+  def read_file filename, create
+    if create && !File.exist?(filename)
+      File.open(filename, 'w') do |file|
+        (RiCal::Component::Calendar.new).export_to file
+      end
+    end
     @calendars << File.open(filename, 'r') do |file|
       RiCal.parse file
     end.flatten.map do |calendar|
@@ -59,15 +69,33 @@ class EventCalendar
     events.flatten
   end
 
+  # Find (non-recuring) events that begin, end or cover the given day.
   def find_events date
     #events = @calendars.map &:events
     @calendars.map do |calendar|
       calendar.events.select { |event|
-        event.dtstart.to_date == date || event.dtend.to_date == date ||!event.occurrences(:overlapping => [date, date +1]).empty?
+        # If end-date is a Date (vs DateTime) let it be
+        # All day/multiple day events
+        if event.dtstart.class == Date && event.dtend.class == Date
+          event.dtstart.to_date == date
+        else
+          event.dtstart.to_date == date || event.dtend.to_date == date
+          # occurrences need to be re-enabled
+          #||!event.occurrences(:overlapping => [date, date +1]).empty?
+        end
       }
     end.flatten.map do |event|
       Event.new event
     end
+    # check flat_map enumerable method
+  end
+
+  def find_by_uid uuid
+    # we want to pick only the first! whats the method? detect is one, find another
+    @calendars.map(&:events).flatten.each do |event|
+      return Event.new(event) if event.uid == uuid
+    end
+    nil
   end
 
   private
@@ -77,13 +105,13 @@ class EventCalendar
   end
 
   def dtmonth_end year, month
-    puts "last date for #{year} - #{month}"
+    #puts "last date for #{year} - #{month}"
     last_day = Date.civil(year, month, -1)
     Icalendar::Values::Date.new('20120102')#"#{year}#{month}#{last_day}")
   end
 
   def date_between? date, start_date, end_date
-    puts "d #{date}  st #{start_date} e #{end_date}"
+    #puts "d #{date}  st #{start_date} e #{end_date}"
     date > start_date && date < end_date
   end
 
@@ -92,7 +120,8 @@ class EventCalendar
   end
 
   def event_includes? event, date
-    event.dtstart.class == event.dtend.class && event.dtstart.class ==  Icalendar::Values::DateTime && (date_between?(date, event.dtstart, event.dtend))
+    incl = event.dtstart.class == event.dtend.class && event.dtstart.class ==  Icalendar::Values::DateTime && (date_between?(date, event.dtstart, event.dtend))
+    incl
   end
 end
 
